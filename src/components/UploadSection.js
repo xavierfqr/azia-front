a33ea9ae0cd4546396d2d95fbd3b08fd8c9b22f7
David Ghiassi <david.ghiassi@gmail.com>
2022-01-16 21:57:46 +0100

    Done upload URL

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Input } from '@mui/material';
 import axios from 'axios';
 
-const config = {
+const configForFile = {
     headers: {
         'Prediction-Key': '09b3429243b5486aadf5794f653d867c',
         'Content-Type': 'application/octet-stream',
@@ -10,12 +10,24 @@ const config = {
     }
 };
 
+const configForUrl = {
+    headers: {
+        'Prediction-Key': '09b3429243b5486aadf5794f653d867c',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    }
+}
+
 function UploadSection() {
 
-    const [file, setFile] = React.useState('')
+    const [file, setFile] = React.useState('');
+    const [result, setResult] = React.useState('');
+    const [URL, setURL] = React.useState('');
 
     const handleFile = () => {
-        if (!file) return;
+        if (!file)
+            return;
+
         const fileReader = new FileReader()
         fileReader.readAsArrayBuffer(file)
 
@@ -26,16 +38,66 @@ function UploadSection() {
             axios.post(
                 'https://remy94550-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/002ee240-3216-4f78-9203-fad70657e559/classify/iterations/Iteration2/image',
                 file,
-                config
-            ).then(res => console.log(res.data))
+                configForFile
+            ).then(res => {
+                console.log(res.data.predictions)
+                const predictions = res.data.predictions;
+
+                var withMask;
+                if (predictions[0].tagName === "with_mask")
+                    withMask = 0;
+                else
+                    withMask = 1;
+
+                if (res.data.predictions[withMask].probability > 0.5)
+                    setResult("Cette personne a un masque.");
+                else
+                    setResult("Cette personne n'a pas de masque.")
+            })
             .catch(err => console.log('an error occurred : ', err));
         }
     }
 
+    const handleChange = (event) => {
+        setURL(event.target.value)
+    }
+
+    const handleURL = () => {
+        axios.post(
+            'https://remy94550-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/002ee240-3216-4f78-9203-fad70657e559/classify/iterations/Iteration2/url',
+            {"Url": URL},
+            configForUrl
+        ).then(res => {
+            console.log(res.data.predictions)
+            const predictions = res.data.predictions;
+
+            var withMask;
+            if (predictions[0].tagName === "with_mask")
+                withMask = 0;
+            else
+                withMask = 1;
+
+            if (res.data.predictions[withMask].probability > 0.5)
+                setResult("Cette personne a un masque.");
+            else
+                setResult("Cette personne n'a pas de masque.")
+        })
+        .catch(err => console.log('an error occurred : ', err));
+    }
+
     return (
         <div>
-            <Input type="file" onChange={(e) => {console.log(e); setFile(e.target.files[0])}}/>
-            <button onClick={handleFile}>Analyse file</button>
+            <div>
+                {result}
+            </div>
+            <div>
+                <Input type="file" onChange={(e) => {console.log(e); setFile(e.target.files[0])}}/>
+                <button onClick={handleFile}>Analyse file</button>
+            </div>
+            
+            <input type="text" value={URL} onChange={handleChange}>
+            </input>
+            <button onClick={handleURL}>Envoyer le lien</button>
         </div>
     )
 }
